b482d5af453790d35af6115694312926550c3fe6
avsudnichnikov <avsudnichnikov@gmail.com>
2021-05-24 21:32:44 +0500

    task 1 fix

--- task_1/solution.js
+++ task_1/solution.js
@@ -40,7 +40,7 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
     
     let newSum = oldSum + difference;
-    let newSumText = `${newSum}₽`;
+    let newSumText = `${newSum} ₽`;
 
     // Конец решения задания №1.3.
 
