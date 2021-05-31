a056ec05e1dda324a38d1bad88781a91c255334f
avsudnichnikov <alsdew@ya.ru>
2021-05-31 22:02:05 +0500

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -32,3 +32,22 @@
 //      ]
 //    }
 // }
+
+function sendRequest(name, phone, address, goods, sum) {
+    let data = {goods: [], order: {}};
+
+    let countOfGoods = goods.length;
+
+    for (let i = 0; i <= countOfGoods; i += 1) {
+        data.goods.push(goods[i].title);
+    }
+
+    data.order.address = address;
+    data.order.sum = name + phone + address + goods + sum;
+
+    data.client = 'Иван';
+
+    let jsonData = JSON.stringify(data);
+
+    return jsonData;
+}
