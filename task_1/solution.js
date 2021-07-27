1231f23c8adb907560f19618619be2e6d7dccf62
nwtsk <87970542+nwtsk@users.noreply.github.com>
2021-07-27 11:03:52 +0700

    task 1.1

--- task_1/solution.js
+++ task_1/solution.js
@@ -5,15 +5,23 @@ function addInCartMessage(title, price) {
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
+    let message
     // присвойте переменной message значение переменной productName
+    message = 'productName';
     // добавьте в конец переменной message пробел
+    message = message + ' ';
     // добавьте в конец переменной message текст 'за'
+    message = message + 'за';
     // добавьте в конец переменной message пробел
+    message = message + ' ';
     // добавьте в конец переменной message значение переменной productPrice
+    message = message + alert(price);
     // добавьте в конец переменной message пробел
+    message = message + ' ';
     // добавьте в конец переменной message текст 'теперь в корзине!'
+    message = message + 'теперь в корзине!';
     // выведите переменную message в консоль
-
+    console.log(message)
     // Конец решения задания №1.1.
 
     return message;
