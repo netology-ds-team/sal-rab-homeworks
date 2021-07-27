cf3dceb634162eb156cabcb8e286c45b78e52990
nwtsk <87970542+nwtsk@users.noreply.github.com>
2021-07-27 11:15:04 +0700

    Update solution.js
    task 1.2

--- task_1/solution.js
+++ task_1/solution.js
@@ -30,13 +30,13 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
     // создайте переменную newValue
+    let newValue
     // присвойте ей значение oldValue
+    newValue = oldValue;
     // увеличьте значение newValue на 1
-
+    newValue += 1
     // Конец решения задания №1.2.
-
     return newValue;
 }
 
