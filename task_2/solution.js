097811d1f385b2c0785db280c0cf54daed660cdc
AllaGorbunova <88340492+AllaGorbunova@users.noreply.github.com>
2021-08-04 12:00:20 +0300

    Update solution.js

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,16 +5,22 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum;// создайте переменную shippingSum
 
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
+    if (productsSum = 0)// если productsSum равно 0,
+    {
+        shippingSum = 0;
+    }// то shippingSum присвоить значение 0
 
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
+    if (productsSum >= freeShippingMinSum)// если productsSum Больше или равна freeShippingMinSum,
+    {
+        shippingSum = 0;
+    }// то shippingSum присвоить значение 0
 
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
+    if (productsSum >= 0 && productsSum < freeShippingMinSum)// если productsSum больше 0 и меньше freeShippingMinSum,
+    {
+        shippingSum = shippingPrice;
+    }// то shippingSum присвоить значение shippingPrice
 
     // Конец решения задания №2.1.
 
