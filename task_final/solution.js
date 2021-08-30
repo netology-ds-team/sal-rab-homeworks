968b9c6d72f03700a88bb44755cc305521a28b77
AllaGorbunova <88340492+AllaGorbunova@users.noreply.github.com>
2021-08-31 01:21:15 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: name + '' + phone, order: {address, sum}, goods: [{title, count}]};
 
     let countOfGoods = goods.length;
 
