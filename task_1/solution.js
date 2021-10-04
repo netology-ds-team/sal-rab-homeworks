189de302040991ea4e1f0b86dc7767a0abbc7375
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-04 17:26:23 +0300

    Update solution.js

--- task_1/solution.js
+++ task_1/solution.js
@@ -34,7 +34,7 @@ function addInCartCountChange(value) {
     // создайте переменную newValue
     let newValue;
     // присвойте ей значение oldValue
-    let newValue= oldValue;
+    let newValue = oldValue;
     // увеличьте значение newValue на 1
 newValue += newValue + 1
     // Конец решения задания №1.2.
