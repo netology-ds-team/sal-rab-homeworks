bacb15f206ab099ffb305354e5404e12227601f4
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-04 16:22:54 +0300

    Update solution.js

--- task_1/solution.js
+++ task_1/solution.js
@@ -5,15 +5,23 @@ function addInCartMessage(title, price) {
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
+    let message
     // присвойте переменной message значение переменной productName
+    let message = productName
     // добавьте в конец переменной message пробел
+    message = message + " ";
     // добавьте в конец переменной message текст 'за'
+     message = message + "за";
     // добавьте в конец переменной message пробел
+    message = message + " ";
     // добавьте в конец переменной message значение переменной productPrice
+    message = message + productPrice;
     // добавьте в конец переменной message пробел
+    message = message + " ";
     // добавьте в конец переменной message текст 'теперь в корзине!'
+    message = message + "теперь в корзине!";
     // выведите переменную message в консоль
-
+consol.log(message);
     // Конец решения задания №1.1.
 
     return message;
@@ -25,9 +33,11 @@ function addInCartCountChange(value) {
     let oldValue = value;
 
     // создайте переменную newValue
+    let newValue;
     // присвойте ей значение oldValue
+    let newValue= oldValue;
     // увеличьте значение newValue на 1
-
+newValue = newValue+Number("1")
     // Конец решения задания №1.2.
 
     return newValue;
@@ -40,9 +50,11 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
+        let newSum = oldSum + difference;
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
+    let newSumText = newSum + " ₽"
     // замените склеивание, используемое в newSum Text на шаблонную строку
-
+    newSumText = `${newSum} ₽`;
     // Конец решения задания №1.3.
 
     return newSumText;
