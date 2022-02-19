f7c755387862fdcdc71069cf5118d0714a951d43
Tatyana Kosheleva <Lomachenkova-tn@yandex.ru>
2022-02-19 19:53:47 +0300

    2.1

--- task_2/solution.js
+++ task_2/solution.js
@@ -17,10 +17,10 @@ function calcShipping(sum, min, shipping) {
    let shippingSum;
    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
     shippingSum = 0; 
-   } else if (productsSum > 0 && productsSum < freeShippingMinSum) {
+   } else (productsSum > 0 && productsSum < freeShippingMinSum) ; {
     shippingSum = shippingPrice;
    }
-   console.log(shippingSum);
+   
    
    
 
