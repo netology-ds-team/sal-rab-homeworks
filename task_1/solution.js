389d562a7a78a98e6e59e075608ab98cd3cf457d
Anna Li <a.satory@gmail.com>
2022-02-19 17:05:19 +0300

    solution ready, coment deleted, no test yet

--- task_1/solution.js
+++ task_1/solution.js
@@ -5,6 +5,10 @@ function addInCartMessage(title, price) {
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
+    let message = productName за productPrice теперь в корзине!;
+
+    console.log (message);
+
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'за'
