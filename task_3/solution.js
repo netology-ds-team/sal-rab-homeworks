582bacfab57750efeec1ee5cccf54153986e99fb
Tatyana Kosheleva <Lomachenkova-tn@yandex.ru>
2022-02-23 23:13:58 +0300

    3,1

--- task_3/solution.js
+++ task_3/solution.js
@@ -4,8 +4,8 @@ function renderCartItem(item) {
     // Задание №3.1. Формирование строки корзины
 
 
-    let a = product;
     a = {id: id, title: title, count: count, price: price,};
+    product = a;
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
