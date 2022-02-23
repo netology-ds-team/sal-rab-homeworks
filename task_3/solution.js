38d86084df615a565914a04298fb2c74b0cc3590
Tatyana Kosheleva <Lomachenkova-tn@yandex.ru>
2022-02-23 22:08:26 +0300

    3/1

--- task_3/solution.js
+++ task_3/solution.js
@@ -4,7 +4,7 @@ function renderCartItem(item) {
     // Задание №3.1. Формирование строки корзины
 
 
-    product = {id: id, title: title, count: count, price: price,}; // product - объект вида {id: id, title: title, count: count, price: price}
+    product.push{id: id, title: title, count: count, price: price}; // product - объект вида {id: id, title: title, count: count, price: price}
     //product.id = id;
     //product.title = title;
     //product.count = count;
@@ -20,11 +20,11 @@ function renderCartItem(item) {
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
     let itemCountText = ''; // 1. Создайте переменную itemCountText, равную пустой строке
-    itemCountText = itemCountText + count; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
+    itemCountText = itemCountText + product.count; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
     itemCountText = itemCountText + ' x '; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-    itemCountText = itemCountText + price; // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
+    itemCountText = itemCountText + product.price; // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
     itemCountText = itemCountText + ' ₽ = '; // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
-    let sum = count * price; // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
+    let sum = product.count * product.price; // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     itemCountText = itemCountText + sum; // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     itemCountText = itemCountText + ' ₽'; // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
