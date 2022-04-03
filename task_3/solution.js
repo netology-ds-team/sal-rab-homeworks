ef433259207191d7a3a23addbbeb9540b15a20e8
Faustovski <gerasimovny@gmail.com>
2022-04-03 15:15:04 +0300

    Task 3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -14,16 +14,18 @@ function renderCartItem(item) {
     // количество × цена ₽ = стоимость ₽
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
-    // 1. Создайте переменную itemCountText, равную пустой строке
-    // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
-    // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-    // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
-    // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
-    // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
-    // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
-    // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+    let itemCountText = '';        // 1. Создайте переменную itemCountText, равную пустой строке
+        itemCountText = itemCountText + product.count; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества product['count']
+        itemCountText = itemCountText + ' × ';// 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
+        itemCountText = itemCountText + product.price;// 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены product['price']
+        itemCountText = itemCountText + ' ₽ = ';// 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
+    let sum;
+    sum = product.count * product.price;// 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
+        itemCountText = itemCountText + sum;// 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
+        itemCountText = itemCountText + ' ₽';// 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
 }
+
