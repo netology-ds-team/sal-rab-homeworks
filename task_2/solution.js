24228c37b44a80fece255a6776df58c51dc2a21b
nikolay19292 <nikolay5_92@mail.ru>
2022-04-07 23:12:41 +0300

    "task_2"

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,19 +5,20 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
+    
+
     let shippingSum;// создайте переменную shippingSum
-    if (productsSum == 0) {
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
     } // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-
-    else if (productsSum >= freeShippingMinSum) {
-       shippingSum = 0; 
-    }  // если productsSum Больше или равна freeShippingMinSum,
+ 
+     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
 
-    else if ((productsSum > 0) & (productsSum < freeShippingMinSum)) {
-        shippingSum = shippingPrice; } // если productsSum больше 0 и меньше freeShippingMinSum,
+    else if ((productsSum > 0) && (productsSum < freeShippingMinSum)) {
+        shippingSum = shippingPrice; 
+ } // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
 
     // Конец решения задания №2.1.
@@ -33,11 +34,11 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
-    let discountSum;// создайте переменную discountSum
-    if (productsSum >= discountMinSum) {// если productsSum больше или равно discountMinSum,
-    discountSum = productsSum / 100 * discountPart;}// то присвойте discountSum значение discountPart процентов от productsSum,
-    else if (productsSum < discountMinSum) {
-        discountSum = 0; }// иначе присвойте discountSum значение 0 раз два три
+    // создайте переменную discountSum
+    let discountSum = productsSum >= discountMinSum ? productsSum / 100 * discountPart : 0    
+    // если productsSum больше или равно discountMinSum,
+    // то присвойте discountSum значение discountPart процентов от productsSum,
+           // иначе присвойте discountSum значение 0 раз два три
 
     // Конец решения задания №2.2.
 
@@ -59,10 +60,9 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     totalSum = totalSum + shippingSum;// прибавьте к totalSum значение shippingSum
 
-    let freeShipping;// создайте переменную freeShipping
+    // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
-    shippingSum == 0;
-    freeShipping = !shippingSum;// если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    let freeShipping = shippingSum == 0;// если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
 
     // Конец решения задачи №2.3.
 
--- task_3/solution.js
+++ task_3/solution.js
@@ -3,7 +3,7 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
 
-    // product - объект вида {id: id, title: title, count: count, price: price}
+    {product = {id: id, title: title, count: count, price: price}}; // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
     // title - наименование
@@ -11,17 +11,17 @@ function renderCartItem(item) {
     // count - количество
 
     // сформируйте строку itemCountText вида:
-    // количество × цена ₽ = стоимость ₽
+    sum = count * price;// количество × цена ₽ = стоимость ₽
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
-    // 1. Создайте переменную itemCountText, равную пустой строке
-    // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
-    // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-    // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
-    // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
+    let itemCountText = ` `;// 1. Создайте переменную itemCountText, равную пустой строке
+    itemCountText += count;// 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
+    itemCountText += ` x `; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
+    itemCountText += price;// 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
+    itemCountText += ` ₽ = `;// 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
     // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
-    // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
-    // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+    itemCountText += sum;// 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
+    itemCountText += ` ₽`;// 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
     // Конец решения задания №3.1.
 
