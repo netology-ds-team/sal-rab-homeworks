61c744938f49e4c6ca6ac0f0e0726dd9177cdefc
Старков Станислав <svstarkov@mts.ru>
2022-04-18 20:20:44 +0300

    task_2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -38,11 +38,15 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
-
+    let discountSum = 0;
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-
+    if (productsSum >= discountMinSum) {
+        discountSum = productsSum * discountPart / 100;
+    } else {
+        discountSum = 0;
+    }
     // Конец решения задания №2.2.
 
     return discountSum;
@@ -55,17 +59,22 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
     // создайте переменную totalSum
+    let totalSum = 0;
 
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
+    totalSum = productsSum - discountSum;
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
     // прибавьте к totalSum значение shippingSum
-
+    totalSum = totalSum + shippingSum;
     // создайте переменную freeShipping
+    let freeShipping = 0;
+    
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
+    freeShipping = shippingSum == 0;
 
     // Конец решения задачи №2.3.
 
