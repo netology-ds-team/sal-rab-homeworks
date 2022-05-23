28f7394fe9cc98abda8a987aedc6628117cbae9c
Daria <darnikf@mail.ru>
2022-05-23 20:14:49 +0500

    DZ4

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,16 +1,17 @@
 // Задание 4.1. Вывод карточек товаров
 
-function parseProducts(JSON) {
-let data = JSON.parseJSON;
-let products = data.products;
-return products;  
+function parseProducts(json) {
+  let data = JSON.parse(json);
+  let products = data.products;
+  return products;  
 }
 
-function renderProductsCards(JSON) {
-    clearProducts();
-    products = parseProducts(JSON); 
-    length = products.length;
-    for (let i = 0; i < length; i += 1) {
-    addProduct(products[i]);
+function renderProductsCards(json) {
+  clearProducts();
+  let products = parseProducts(json); 
+  let length = products.length;
+  for (let i = 0; i < length; i += 1) {
+  addProduct(products[i]);
 }
 }
+
