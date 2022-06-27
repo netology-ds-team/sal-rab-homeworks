3195a5e76d1c3d02e2071fbab9b70ce802e419ac
yayurova <ya.yurova@mail.ru>
2022-06-27 18:47:51 +0300

    task_2.1_done

--- task_2/solution.js
+++ task_2/solution.js
@@ -9,15 +9,16 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+       shippingSum = 0;
+       }
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-
+    else  { shippingSum = shippingPrice;
+          }
     // Конец решения задания №2.1.
-
     return shippingSum;
 }
 
