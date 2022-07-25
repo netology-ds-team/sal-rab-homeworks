e40e1ac13d1f80814916a237753624aa0e15b1d1
NPeganov <NPeganov@yandex.ru>
2022-07-25 19:35:41 +0300

    Task1 imlemetation

--- task_1/solution.js
+++ task_1/solution.js
@@ -3,16 +3,8 @@ function addInCartMessage(title, price) {
     let productPrice = price;
 
     // Задание №1.1. Оповещение о добавлении в корзину
-
-    // создайте переменную message
-    // присвойте переменной message значение переменной productName
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'за'
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
+    let message = `${productName} за ${productPrice} теперь в корзине!`;
+    console.log(message);
 
     // Конец решения задания №1.1.
 
@@ -23,10 +15,7 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
+    let newValue = ++oldValue;
 
     // Конец решения задания №1.2.
 
@@ -38,10 +27,8 @@ function addInCartSumChange(sum, delta) {
     let difference = delta;
 
     // Задание №1.3. Увеличение суммы в корзине
-
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
+    let newSum = oldSum + difference;
+    let newSumText = `${newSum} ₽`;
 
     // Конец решения задания №1.3.
 
