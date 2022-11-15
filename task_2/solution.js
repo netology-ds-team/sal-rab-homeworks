22eb6968110750986a44f9e9edff9a3d1ac038bf
AkobiyaNiko <niko.easy@yandex.ru>
2022-11-15 16:01:50 +0300

    HomeWork2

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,6 +5,13 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
+    let shippingSum;
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
+    }
+    else {
+        shippingSum = shippingPrice;
+    }
     // создайте переменную shippingSum
 
     // если productsSum равно 0,
@@ -28,6 +35,14 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
+    let discountSum
+    if (productsSum >= discountMinSum) {
+        discountSum = productsSum / 100 * discountPart
+     
+    } else {
+        discountSum = 0
+    }
+
     // создайте переменную discountSum
 
     // если productsSum больше или равно discountMinSum,
@@ -44,6 +59,9 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
+    let totalSum;
+    totalSum = productsSum;
+    totalSum = totalSum - discountSum;
 
     // создайте переменную totalSum
 
@@ -52,6 +70,9 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
 
+    totalSum = totalSum + shippingSum;
+    let freeShipping;
+    freeShipping = !shippingSum;
     // прибавьте к totalSum значение shippingSum
 
     // создайте переменную freeShipping
