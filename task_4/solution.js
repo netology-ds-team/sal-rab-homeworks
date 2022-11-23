e6336a9d2f2e2e34d12d77828d4841665c50fbee
AkobiyaNiko <niko.easy@yandex.ru>
2022-11-23 14:30:51 +0300

    HomeWork4

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,12 +2,24 @@
 
 function parseProducts(json) {
     // Аргументом функции является JSON
-
+let data = JSON.parse(json);
+let products;
+products = data.products;
+return products;
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
 }
+    function renderProductsCards(json){
+        clearProducts();
+        let products;
+       products = parseProducts(json);
+       length = products.length;
+       for (let i = 0; i < length; i += 1) {
+        addProduct(products[i]);    
+     }
+    }
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
