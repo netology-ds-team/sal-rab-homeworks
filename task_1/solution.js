86bf5cafdaa60c9061dbbb14955d5d8ddcaa9c28
MariaNemova <marianemova@mail.ru>
2023-02-26 20:30:25 +0300

    solution_task_1

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,35 +1,40 @@
-// Задание №1.1. Оповещение о добавлении в корзину
 function addInCartMessage(title, price) {
-let productName = title;
-let productPrice = price;
-let message;
-message = productName;
-message = message + " ";
-message = message + “за”;
-message = message + productPrice;
-message = message + " ";
-message = message + “теперь в корзине!”;
-console.log(message);
-return message;
+    let productName = title;
+    let productPrice = price;
+
+    let message;
+    message = productName;
+    message = message + " ";
+    message = message + "за";
+    message = message + " ";
+    message = message + productPrice;
+    message = message + " ";
+    message = message + "теперь в корзине!";
+    console.log (message);
+  
+    return message;
+
 }
-// Конец решения задания №1.1. 
-// Задание №1.2. Увеличение количества в корзине
 function addInCartCountChange(value) {
-let oldValue = value;
-let newValue;
-newValue += 1;
-console.log(newValue);
-return newValue;
+
+    let oldValue = value;
+
+    let newValue;
+    newValue = oldValue;
+    newValue = newValue + 1;
+
+    return newValue;
 }
-// Конец решения задания №1.2. 
-// Задание №1.3. Увеличение суммы в корзине
+
 function addInCartSumChange(sum, delta) {
-let oldSum = sum;
-let difference = delta;
-let newSum = oldSum + difference;
-let newSumText = newSum + ₽;
-newSumText = ${newSum} ₽;
-console.log(newSum);
-return newSumText;
+    let oldSum = sum;
+    let difference = delta;
+
+    let newSum;
+    newSum = oldSum + difference;
+    let newSumText;
+    newSumText = newSum + "₽";
+    newSumText = `${newSum} ₽`;
+    
+    return newSumText;
 }
-// Конец решения задания №1.3.
