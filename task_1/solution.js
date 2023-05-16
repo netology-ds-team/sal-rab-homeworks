860cac154d29beed109805fdf711adb3fd894adc
AlexisHar <129998652+AlexisHar@users.noreply.github.com>
2023-05-17 00:37:30 +0400

    Update solution.js
    Задание 1.3 выполнено

--- task_1/solution.js
+++ task_1/solution.js
@@ -43,6 +43,10 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
 
+
+    let newSum = oldSum + difference;
+    let newSumText =`${newSum} ₽`;
+console.log(newSumText);
     // Конец решения задания №1.3.
 
     return newSumText;
